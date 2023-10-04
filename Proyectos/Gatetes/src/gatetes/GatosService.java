c2adec6b14a81e4c7d623e0adb013e747ff817c0
xtalo <117736097+santiagoieshna@users.noreply.github.com>
2023-10-04 21:41:57 +0200

    practica de groupByin

--- Proyectos/Gatetes/src/gatetes/GatosService.java
+++ Proyectos/Gatetes/src/gatetes/GatosService.java
@@ -1,22 +1,33 @@
 package gatetes;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class GatosService {
 	private GatosRepository gatosRepository;
 
 	public GatosService(GatosRepository gatosRepository) {
 		super();
-		this.gatosRepository=gatosRepository;
+		this.gatosRepository = gatosRepository;
 	}
-	
+
 	public void addOneThousandCats() {
-		GatoOM gatoOM=new GatoOM();
+		GatoOM gatoOM = new GatoOM();
 		int maximoGatos = 1000;
 		do {
-			gatosRepository.addGato(gatoOM.getRandomCat());				
-		}while(gatosRepository.size()<maximoGatos);
+			gatosRepository.addGato(gatoOM.getRandomCat());
+		} while (gatosRepository.size() < maximoGatos);
 	}
-	
+
 	// Quiero que me digas cuantos gatos hay por cada raza
-	
-	
+	public Map<Raza, Long> getMeowsPorRaza() {
+
+		Map<Raza, Long> razasGato = new HashMap<>();
+
+		
+
+		return razasGato;
+	}
+
 }
